212cec76ecbf5c1688616726083738d6072f248a
Neha Thakor <neha.k.thakor@gmail.com>
2024-01-23 11:31:21 +0530

    second commit

--- revision02/src/App.jsx
+++ revision02/src/App.jsx
@@ -1,8 +1,21 @@
-function App() {
+import { useEffect, useState } from "react"
 
+function App() {
+  const [value,setValue] = useState(0);
+  const[data,setData] = useState("Ram");
+  useEffect(()=>{
+    console.log("button clicked")
+  },[data])
+  
   return (
     <>
-     <p>Hello world</p>
+      <h3>Button clicked {value} times</h3>
+      <button onClick={() => {
+        setValue(value+1)
+      }}>Click me</button>
+      <button onClick={() => {
+        setData("Sita")
+      }}>Click me</button>
     </>
   )
 }
